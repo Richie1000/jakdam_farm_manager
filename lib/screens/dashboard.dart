8e2ea0a56fa4a4e832b8499810fed335ff840229
Richie <esselrichmond78@gmail.com>
2024-09-30 15:57:56 +0000

    Redesigning Complete

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,138 +1,205 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
+import '../providers/auth.dart' as customAuth;
+import 'feed_calculation_screen.dart';
+import 'feed_formulae_screen.dart';
+import 'inventory_screen.dart';
+import 'pond_calculation_screen.dart';
 
-import '../providers/auth.dart';
-import './feed_calculation_screen.dart';
-import './pond_calculation_screen.dart';
-import './feed_formulae_screen.dart';
-import './inventory_screen.dart';
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({Key? key}) : super(key: key);
 
-class DashboardScreen extends StatelessWidget {
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  String _username = '';
+  String _initials = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUsernameAndInitials();
+  }
+
+  Future<void> _fetchUsernameAndInitials() async {
+    // Use the custom AuthProvider with prefix
+    final user = Provider.of<customAuth.AuthProvider>(context, listen: false).user;
+
+    if (user != null) {
+      // Fetch user data from Firestore
+      final userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+
+      if (userDoc.exists) {
+        final fullName = userDoc['username'] as String;
+
+        // Split the name and get initials
+        List<String> names = fullName.trim().split(' ');
+
+        setState(() {
+          _username = names[0]; // Set first name for greeting
+          if (names.length > 1) {
+            _initials = names[0][0].toUpperCase() + names[1][0].toUpperCase();
+          } else {
+            _initials = names[0].substring(0, 2).toUpperCase();
+          }
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // ignore: deprecated_member_use
-    return WillPopScope(
-      onWillPop: () async {
-        // Close the app when back is pressed
-        SystemNavigator.pop(); 
-        return false; // Prevents the default back action
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Jakdam'),
-          automaticallyImplyLeading: false, // Removes the back button
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(2),
-          child: GridView.count(
-            crossAxisCount: 2,
-            mainAxisSpacing: 5,
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          // Header Container
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(50),
+              ),
+            ),
+            child: Column(
+              children: [
+                const SizedBox(height: 50),
+                ListTile(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 30),
+                  title: Text(
+                    'Hello $_username!',
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+                  subtitle: Text(
+                    'Good Morning',
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: Colors.white54,
+                        ),
+                  ),
+                  trailing: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Theme.of(context).primaryColorLight,
+                    child: Text(
+                      _initials,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+              ],
+            ),
+          ),
+          // Dashboard Items Container
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(200)),
+              ),
+              child: GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                crossAxisSpacing: 40,
+                mainAxisSpacing: 30,
+                children: [
+                  itemDashboard('Pond Calculations', 'assets/animations/pond.json', context, () { 
+                    Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>  PondCalculationsScreen()),
+                  );
+                   }),
+                  itemDashboard('Feed Calculations', 'assets/animations/feed.json', context, () { 
+                    Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const FeedCalculationScreen()),
+                  );
+                   }),
+                  itemDashboard('Inventory', 'assets/animations/stock.json', context, () { 
+                    Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) =>  InventoryScreen()),
+                  );
+                   }),
+                  itemDashboard('Feed Formulae', 'assets/animations/group.json', context, () { 
+                    Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const FeedFormulaeScreen()),
+                  );
+                   }),
+                  itemDashboard('Request Training', 'assets/animations/training.json', context, () async { 
+                    final Uri url = Uri.parse('https://jakdamfarmlife.com');
+                    if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+                      throw 'Could not launch $url';
+                    }
+                   }),
+                  itemDashboard('Logout', 'assets/animations/logout.json', context, () {  
+                    final authProvider = Provider.of<customAuth.AuthProvider>(context, listen: false);
+                    authProvider.signOut();
+                  }),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+
+  // Function to create dashboard items with onTap handling
+  Widget itemDashboard(String title, String lottieFile, BuildContext context, VoidCallback onTap) => GestureDetector(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                offset: const Offset(0, 5),
+                color: Theme.of(context).primaryColor.withOpacity(.2),
+                spreadRadius: 2,
+                blurRadius: 5,
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              DashboardCard(
-                title: 'Pond Calculations',
-                lottieAsset: 'assets/animations/pond.json',
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => PondCalculationsScreen()),
-                  );
-                },
+              Container(
+                padding: const EdgeInsets.all(20),
+                height: 100,
+                width: 100,
+                child: Lottie.asset(lottieFile), // Display the Lottie animation
               ),
-              DashboardCard(
-                title: 'Feed Calculations',
-                lottieAsset: 'assets/animations/feed.json',
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => FeedCalculationScreen()),
-                  );
-                },
-              ),
-              DashboardCard(
-                title: 'Inventory',
-                lottieAsset: 'assets/animations/stock.json',
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => InventoryScreen()),
-                  );
-                },
-              ),
-              DashboardCard(
-                title: 'Feed Formulae',
-                lottieAsset: 'assets/animations/group.json',
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => FeedFormulaeScreen()),
-                  );
-                },
-              ),
-              DashboardCard(
-                title: 'Request Training',
-                lottieAsset: 'assets/animations/training.json',
-                onTap: () async {
-                  final Uri url = Uri.parse('https://jakdamfarmlife.com');
-                  if (!await launchUrl(url,
-                      mode: LaunchMode.externalApplication)) {
-                    throw 'Could not launch $url';
-                  }
-                },
-              ),
-              DashboardCard(
-                title: 'Logout',
-                lottieAsset: 'assets/animations/logout.json',
-                onTap: () {
-                  // Handle logout action
-                  final authProvider =
-                      Provider.of<AuthProvider>(context, listen: false);
-                  authProvider.signOut();
-                },
+              const SizedBox(height: 8),
+              Text(
+                title.toUpperCase(),
+                style: Theme.of(context).textTheme.titleMedium,
               ),
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class DashboardCard extends StatelessWidget {
-  final String title;
-  final String lottieAsset;
-  final VoidCallback onTap;
-
-  DashboardCard(
-      {required this.title, required this.lottieAsset, required this.onTap});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Card(
-        elevation: 4.0,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Lottie.asset(
-              lottieAsset,
-              width: 100,
-              height: 100,
-            ),
-            SizedBox(height: 10),
-            Text(
-              title,
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+      );
 }
